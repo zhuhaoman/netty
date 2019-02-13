af38e653697708754a363546a63460d9f6782222
zhuhm <youxiazhm@guoxiaomei.com>
2019-02-14 00:05:17 +0800

    first commit

---
+++ chapter2/server/src/main/java/com/zhuhaoman/netty/chapter2/server/EchoServer.java
@@ -0,0 +1,9 @@
+package com.zhuhaoman.netty.chapter2.server;
+
+/**
+ * @author zhuhaoman
+ * @description:
+ * @date 2019-02-13 23:09
+ **/
+public class EchoServer {
+}
---
+++ chapter2/server/src/main/java/com/zhuhaoman/netty/chapter2/server/EchoServerHandler.java
@@ -0,0 +1,11 @@
+package com.zhuhaoman.netty.chapter2.server;
+
+/**
+ * @author zhuhaoman
+ * @description:
+ * @date 2019-02-13 22:37
+ **/
+@Sha
+public class EchoServerHandler extends ChannelInboundHandlerAdapter{
+
+}
