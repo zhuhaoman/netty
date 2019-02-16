cc8176a265a83554de077c5001192c50013e6a8e
i01007600819 <zhuhaoman@cathay-ins.com.cn>
2019-02-16 17:23:53 +0800

    'update'

--- chapter2/server/src/main/java/com/zhuhaoman/netty/chapter2/server/EchoServer.java
+++ chapter2/server/src/main/java/com/zhuhaoman/netty/chapter2/server/EchoServer.java
@@ -1,9 +1,56 @@
 package com.zhuhaoman.netty.chapter2.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * @author zhuhaoman
  * @description:
  * @date 2019-02-13 23:09
  **/
 public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage:"+EchoServer.class.getSimpleName()+ " <port>");
+        }
+
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    private void start() throws Exception{
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                   .channel(NioServerSocketChannel.class)
+                   .localAddress(new InetSocketAddress(port))
+                   .childHandler(new ChannelInitializer<SocketChannel>() {
+                       protected void initChannel(SocketChannel ch) throws Exception {
+                           ch.pipeline().addLast(serverHandler);
+                       }
+                   });
+
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
 }
--- chapter2/server/src/main/java/com/zhuhaoman/netty/chapter2/server/EchoServerHandler.java
+++ chapter2/server/src/main/java/com/zhuhaoman/netty/chapter2/server/EchoServerHandler.java
@@ -1,11 +1,39 @@
 package com.zhuhaoman.netty.chapter2.server;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
 /**
  * @author zhuhaoman
  * @description:
  * @date 2019-02-13 22:37
  **/
-@Sha
-public class EchoServerHandler extends ChannelInboundHandlerAdapter{
 
+//@Sharable表示一个ChannelHandler可以被多个channel安全共享
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("server received:" + in.toString(CharsetUtil.UTF_8));
+        //将接收到的消息写给发送者
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
